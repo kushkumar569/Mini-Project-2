8ff9ce7af7ad1d8f8041c065f6330939faa07f2e
kushkumar569 <kushk569@gmail.com>
2025-03-06 23:32:00 +0530

    logo

--- vite.config.js
+++ vite.config.js
@@ -7,53 +7,53 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "prompt",
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon-512x512.png'],
-      manifest: {
-        name: 'My PWA App',
-        short_name: 'PWA App',
-        description: 'My awesome PWA application!',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: "/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          },
-          {
-            src: "/apple-touch-icon.png",
-            sizes: "180x180",
-            type: "image/png",
-            purpose: "any"
-          },
-          {
-            src: "/icon-144x144.png",
-            sizes: "144x144",
-            type: "image/png",
-            purpose: "any"
-          },
-          {
-            src: "/icon-256x256.png",
-            sizes: "256x256",
-            type: "image/png",
-            purpose: "any"
-          },
-          {
-            src: "/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
-            purpose: "any maskable"
-          },
-          {
-            src: "/masked-icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable"
-          }
+      includeAssets: ['logo.png', 'logo.svg'],
+manifest: {
+  name: 'My PWA App',
+  short_name: 'PWA App',
+  description: 'My awesome PWA application!',
+  theme_color: '#ffffff',
+  icons: [
+    {
+      src: "/logo.png",
+      sizes: "192x192",
+      type: "image/png"
+    },
+    {
+      src: "/logo.png",
+      sizes: "512x512",
+      type: "image/png"
+    },
+    {
+      src: "/logo.png",
+      sizes: "180x180",
+      type: "image/png",
+      purpose: "any"
+    },
+    {
+      src: "/logo.png",
+      sizes: "144x144",
+      type: "image/png",
+      purpose: "any"
+    },
+    {
+      src: "/logo.png",
+      sizes: "256x256",
+      type: "image/png",
+      purpose: "any"
+    },
+    {
+      src: "/logo.png",
+      sizes: "384x384",
+      type: "image/png",
+      purpose: "any maskable"
+    },
+    {
+      src: "/logo.svg",
+      type: "image/svg+xml",
+      purpose: "any"
+    }
+
         ]
       }
     })
