485c306b9ebe4f4ae126b1ab0c18410dd3580c37
kushkumar569 <kushk569@gmail.com>
2025-03-06 23:43:07 +0530

    logo

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; 
+import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
@@ -8,51 +8,51 @@ export default defineConfig({
     VitePWA({
       registerType: "prompt",
       includeAssets: ['logo.png', 'logo.svg'],
-manifest: {
-  name: 'My PWA App',
-  short_name: 'PWA App',
-  description: 'My awesome PWA application!',
-  theme_color: '#ffffff',
-  icons: [
-    {
-      src: "/logo.png",
-      sizes: "192x192",
-      type: "image/png"
-    },
-    {
-      src: "/logo.png",
-      sizes: "512x512",
-      type: "image/png"
-    },
-    {
-      src: "/logo.png",
-      sizes: "180x180",
-      type: "image/png",
-      purpose: "any"
-    },
-    {
-      src: "/logo.png",
-      sizes: "144x144",
-      type: "image/png",
-      purpose: "any"
-    },
-    {
-      src: "/logo.png",
-      sizes: "256x256",
-      type: "image/png",
-      purpose: "any"
-    },
-    {
-      src: "/logo.png",
-      sizes: "384x384",
-      type: "image/png",
-      purpose: "any maskable"
-    },
-    {
-      src: "/logo.svg",
-      type: "image/svg+xml",
-      purpose: "any"
-    }
+      manifest: {
+        name: 'Geofancing Attendence Manager',
+        short_name: 'GAM app',
+        description: 'My awesome Geo-fencing Attendence Manager application!',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: "/logo.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/logo.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "/logo.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "/logo.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "/logo.png",
+            sizes: "256x256",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "/logo.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: "/logo.svg",
+            type: "image/svg+xml",
+            purpose: "any"
+          }
 
         ]
       }
