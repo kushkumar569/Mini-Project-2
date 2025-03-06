9a8bb6ceb99dbe8ebd8fbb53956e588e8e16ac6a
kushkumar569 <kushk569@gmail.com>
2025-03-06 22:22:40 +0530

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { LoginSignup } from './components/LoginSignup'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div >Geo-Fencing Attendence Manager</div>
+      <LoginSignup/>
     </>
   )
 }
