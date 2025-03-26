0ff2c3484518fa3683d9d0b8933afbed7a155c9d
kushkumar569 <kushk569@gmail.com>
2025-03-26 22:58:22 +0530

    usenavigate

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,12 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet,useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function PrivateRoute({ allowedRoles }) {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [userRole, setUserRole] = useState(null);
     const [loading, setLoading] = useState(true);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch(`${process.env.BACKEND_URL}/me`, {
@@ -29,7 +31,8 @@ function PrivateRoute({ allowedRoles }) {
     // Redirect if not authenticated or role mismatch
     if (!isAuthenticated || allowedRoles != userRole) {
         console.log(allowedRoles);
-        return <Navigate to="/unauthorized" />;
+        navigate("/unauthorized");
+        // return <Navigate to="/unauthorized" />;
     }
 
     return <Outlet />;
