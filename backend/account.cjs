bdbf92873da4f5c8097c3836392cc8e53ec87a21
kushkumar569 <kushk569@gmail.com>
2025-03-26 22:03:56 +0530

    cors

--- backend/account.cjs
+++ backend/account.cjs
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Enable CORS with specific origin
 app.use(cors({
-    origin: "https://gam0.netlify.app/", // Allow multiple origins
+    origin: "https://gam0.netlify.app", // Allow multiple origins
     credentials: true // Allow cookies to be sent
 }));
 
